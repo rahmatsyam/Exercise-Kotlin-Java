e7392d73bbe38b5252ff5173d3e8b1be4f53f096
rahmatsyam <akyrisa03@gmail.com>
2023-10-31 17:35:26 +0800

    feat: specific element on list

---
+++ src/list/AddList.kt
@@ -0,0 +1,8 @@
+package list
+
+fun main() {
+    val numbers = mutableListOf("one", "five", "six")
+    numbers.add(1, "two")
+    numbers.addAll(2, listOf("three", "four"))
+    println(numbers)
+}
